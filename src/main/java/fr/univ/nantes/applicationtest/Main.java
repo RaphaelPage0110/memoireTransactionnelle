59402cdf21a3e2d4e845ee6ba2a47bb49c203494
Raphael Page <page.raf@gmail.com>
2019-03-30 17:21:32 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/fr/univ/nantes/applicationTest/Main.java
    #	src/main/java/fr/univ/nantes/montecarlo/PiApproximation.java

--- src/main/java/fr/univ/nantes/applicationtest/Main.java
+++ src/main/java/fr/univ/nantes/applicationtest/Main.java
@@ -1,66 +1,66 @@
 package fr.univ.nantes.applicationtest;
 
-import fr.univ.nantes.except.AbortCommitException;
-import fr.univ.nantes.except.AbortReadingException;
-import fr.univ.nantes.impl.ConcreteRegister;
-import fr.univ.nantes.impl.ConcreteTransaction;
-import fr.univ.nantes.inter.Register;
-import fr.univ.nantes.inter.Transaction;
+import fr.univ.nantes.TL2.except.AbortCommitException;
+import fr.univ.nantes.TL2.except.AbortReadingException;
+import fr.univ.nantes.TL2.impl.ConcreteRegister;
+import fr.univ.nantes.TL2.impl.ConcreteTransaction;
+import fr.univ.nantes.TL2.inter.Register;
+import fr.univ.nantes.TL2.inter.Transaction;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 class Main {
 
-    public static void main(String argv[]){
+  public static void main(String argv[]){
 
-        AtomicInteger clock = new AtomicInteger(0);
-        AtomicInteger idCount = new AtomicInteger(0);
-        Register<Integer> partage = new ConcreteRegister<>(clock.get(), 0, idCount.getAndIncrement());
+    AtomicInteger clock = new AtomicInteger(0);
+    AtomicInteger idCount = new AtomicInteger(0);
+    Register<Integer> partage = new ConcreteRegister<>(clock.get(), 0, idCount.getAndIncrement());
 
-        Thread threads[] = new Thread[10000];
+    Thread threads[] = new Thread[10000];
 
-        for (int i=0; i < threads.length ;i++){
-            threads[i] = new Thread(() -> {
-                Transaction<Integer> transaction = new ConcreteTransaction<>(clock);
+    for (int i=0; i < threads.length ;i++){
+      threads[i] = new Thread(() -> {
+        Transaction<Integer> transaction = new ConcreteTransaction<>(clock);
 
-                while(!transaction.isCommitted()) {
+        while(!transaction.isCommitted()) {
 
-                    try {
-                        transaction.begin();
+          try {
+            transaction.begin();
 
-                        int j = partage.read(transaction);
-                        int oldvalue = j;
-                        j++;
-                        partage.write(transaction, j);
-                        partage.read(transaction);
+            int j = partage.read(transaction);
+            int oldvalue = j;
+            j++;
+            partage.write(transaction, j);
+            partage.read(transaction);
 
-                        transaction.try_to_commit();
+            transaction.try_to_commit();
 
-                        System.out.println("Je "+Thread.currentThread().getId()+" lis une valeur de j de "+oldvalue);
-                        System.out.println("Je "+Thread.currentThread().getId()+" écris une valeur de j de "+j);
-                        System.out.println("Je "+Thread.currentThread().getId()+" ai réussi à commit");
-                    } catch(AbortCommitException | AbortReadingException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+            System.out.println("Je "+Thread.currentThread().getId()+" lis une valeur de j de "+oldvalue);
+            System.out.println("Je "+Thread.currentThread().getId()+" écris une valeur de j de "+j);
+            System.out.println("Je "+Thread.currentThread().getId()+" ai réussi à commit");
+          } catch(AbortCommitException | AbortReadingException e) {
+            e.printStackTrace();
+          }
         }
-
-        for (Thread thread : threads) {
-            thread.start();
-        }
-
-        //we make sure all the threads finish
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println(partage.getValue());
+      });
     }
+
+    for (Thread thread : threads) {
+      thread.start();
+    }
+
+    //we make sure all the threads finish
+    for (Thread thread : threads) {
+      try {
+        thread.join();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+
+    System.out.println(partage.getValue());
+  }
 
 
 }
